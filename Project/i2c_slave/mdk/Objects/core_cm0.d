6409912d781d429399e8701d21d6c1030e7bb35c
tom <tom@emakefun.com>
2021-08-11 09:43:15 +0800

    add iic slave demo

--- Project/i2c_slave/mdk/Objects/core_cm0.d
+++
@@ -1,2 +0,0 @@
-.\objects\core_cm0.o: ..\..\..\bsp\core\startup\core_cm0.c
-.\objects\core_cm0.o: E:\keil\ARM\ARMCC\Bin\..\include\stdint.h
--- Project/i2c_slave/mdk/Objects/startup_xinc.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_xinc.o: ..\..\..\bsp\core\startup\startup_xinc.s
